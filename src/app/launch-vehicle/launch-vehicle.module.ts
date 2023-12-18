7b8d47c0a06d232e608778eaf126777a0281516b
Spantrik369 <154117948+Spantrik369@users.noreply.github.com>
2023-12-18 16:54:58 +0530

    First commit for the project

---
+++ src/app/launch-vehicle/launch-vehicle.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LaunchVehicleRoutingModule } from './launch-vehicle-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LaunchVehicleRoutingModule
+  ]
+})
+export class LaunchVehicleModule { }
